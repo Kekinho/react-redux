91f85bfffc949c39eee1004948cc5d874e2f69c6
Eric Bernardes <bernardes.eric@gmail.com>
2023-12-03 08:10:19 -0300

    Testing env variable

--- src/infra/stockapi/data.js
+++ src/infra/stockapi/data.js
@@ -2,7 +2,7 @@
 
 
 export async function fetchStockData() {
-    const url = 'https://script.google.com/macros/s/AKfycbwkLlFuLdRJi9lxl67VABmTNC-zyvAQXp93cAeEVQT1TEyPw2YVfFMVQ2NaEijqlAI3fA/exec';
+    const url = process.env.REACT_APP_STOCK_DATA_URL;
     const response = await fetch(url);
     const data = await response.json();
     return data.acoes;
